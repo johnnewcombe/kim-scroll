b6ce0a1f9b70472ef2e5ac732d393f417687334d
John Newcombe (GlassTTY) <jnewcombeuk@gmail.com>
2024-08-25 19:20:39 +0100

    Added ZP index

--- src/scroll.asm
+++ src/scroll.asm
@@ -21,6 +21,8 @@
 ;=============================================================================
 
             PUT include/kim-symbols.asm
+            PUT include/defs.asm
+            PUT include/macros.asm
 
             MX  %11             ; 11 tells Merlin32 that A, X and Y are
                                 ;   8 bit registers
@@ -36,30 +38,76 @@
             lda #$3f
             sta PBDD
 
+            ;LDA <#DATA          ; set up a vector to point to the data
+            ;STA DATAINDEX
+            ;LDA >#DATA
+            ;STA DATAINDEX + 1
+
+            lda #$00            ; store initial value for Y in zero page
+            sta $00
+
+            LDA #$FF
+            STA $1707          ; set timer division
+
 DISPLOOP
-            ldx #$00              ; initialise character offset
-            ldy #$09              ; init 7 segment offset
+            ldy $00             ; initialise character offset from Zero Page
+            ldx #$09              ; init 7 segment offset
 
 CHARLOOP
             lda #$00
             sta SAD             ; do it this way
-            sty SBD             ;   to remove flicker
-            lda DATA, x
+            stx SBD             ;   to remove flicker
+            ;lda (DATAINDEX),y
+            lda DATA,y
             sta SAD
 
-            txa                 ; delay added to allow the led to fully light
-            ldx #$04            ;   helps a little with the genuine KIM-1
-CHARDELAY
-            dex
-            bne CHARDELAY       ; glow up character
-            tax
 
+;-----------------------------------------------------------------------------
+; delay to allow the leds to reach full brightness
+;-----------------------------------------------------------------------------
+            tya                 ; delay added to allow the led to fully light
+            ldy #$04            ;   helps a little with the genuine KIM-1
+CHARDELAY
+            dey
+            bne CHARDELAY       ; glow up character
+            tay
+;-----------------------------------------------------------------------------
+
+            iny
             inx
-            iny
-            iny
-            cpx #6
+            inx
+            cpx #$15            ; beyond last segment
             bne CHARLOOP
-            jmp DISPLOOP        ; refresh the display
+
+;-----------------------------------------------------------------------------
+; all segments done, check timer, and start again to refresh display
+;-----------------------------------------------------------------------------
+            LDA $1707           ; check the timer
+            BEQ DONE
+            LDA $1706           ; restore divider etc (a read will accomplish this)
+            INC $00             ; increment index low byte
+            ; TODO check for terminator
+
+DONE        jmp DISPLOOP        ; refresh the display
+
+;-----------------------------------------------------------------------------
+; DECINDEX subroutine: decrement 16 bit variable INDEXL/INDEXH
+;-----------------------------------------------------------------------------
+DECINDEX        LDA     DATAINDEX       ;Get index low byte
+                BNE     SKP_IDXH        ;Test for INDEXL = zero
+                DEC     DATAINDEX+1     ;Decrement index high byte
+SKP_IDXH        DEC     DATAINDEX       ;Decrement index low byte
+                RTS                     ;Return to caller
+
+;-----------------------------------------------------------------------------
+; INCINDEX subroutine: increment 16 bit variable INDEXL/INDEXH
+;-----------------------------------------------------------------------------
+INCINDEX        INC     DATAINDEX       ;Increment index low byte
+                BNE     SKP_IDX         ;If not zero, skip high byte
+                INC     DATAINDEX+1     ;Increment index high byte
+SKP_IDX         RTS                     ;Return to caller
+
+
 
 DATA        DB LH, LE, LL, LL, LO, LSPC, LG, LL, LA, LS, LS, LSPC, Lt, Lt, LY
             DB $EA
